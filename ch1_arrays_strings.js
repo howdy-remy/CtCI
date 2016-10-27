7b374fe3237680fcf3ffed00a0f7353ee37bae1d
Kimber Lynch <kmlynch714@gmail.com>
2016-10-27 15:02:17 -0400

    ch1 1.2

--- ch1_arrays_strings.js
+++ ch1_arrays_strings.js
@@ -2,8 +2,32 @@
 function isUnique(string){
 	let dict = {};
 	for(let i = 0; i < string.length; i++){
-		if(!dict[string[i]]) dict[string[i]] = true;
-		else return false;
+		if(!dict[string[i]]) { dict[string[i]] = true; }
+		else { return false; }
+	}
+	return true;
+}
+
+//1.2 Check Permutation (string, string, boolean) => boolean
+function checkPermutation(stringA, stringB, caseSensitive = false){
+	if(!caseSensitive) {
+		stringA = stringA.toLowerCase();
+		stringB = stringB.toLowerCase();
+	}
+	if(stringA.length !== stringB.length) return false;
+	
+	let objA = {};
+
+	for(let i = 0; i < stringA.length; i++){
+		if(!objA[stringA[i]]) { objA[stringA[i]] = 1 }
+		else { objA[stringA[i]]++ }
+	}
+
+	for(let j = 0; j < stringB.length; j++){
+		if(!objA[stringB[j]]) { return false; }
+		else { objA[stringB[j]]-- }
+
+		if (objA[stringB[j]] < 0) { return false }
 	}
 	return true;
 }
